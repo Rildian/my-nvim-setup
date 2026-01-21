b5edbf73ba0e3d2006a3a9ca08861fb3b2485e69
Rildian <130923873+Rildian@users.noreply.github.com>
2026-01-21 15:01:58 -0300

    Enhance debug.lua with key mappings and Python support
    Added key mappings for debugging actions and integrated Python debugging setup.

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -3,28 +3,66 @@ return {
   dependencies = {
     "rcarriga/nvim-dap-ui",
     "nvim-neotest/nvim-nio",
-    "jay-babu/mason-nvim-dap.nvim",
+    "mfussenegger/nvim-dap-python",
   },
-  -- In LazyVim, we configure via keys and config hooks, not a raw setup call
+  keys = {
+    {
+      "<F5>",
+      function()
+        require("dap").continue()
+      end,
+      desc = "Debug: Start/Continue",
+    },
+    {
+      "<F10>",
+      function()
+        require("dap").step_over()
+      end,
+      desc = "Debug: Step Over",
+    },
+    {
+      "<F11>",
+      function()
+        require("dap").step_into()
+      end,
+      desc = "Debug: Step Into",
+    },
+    {
+      "<S-F11>",
+      function()
+        require("dap").step_out()
+      end,
+      desc = "Debug: Step Out",
+    },
+    {
+      "<S-F5>",
+      function()
+        require("dap").terminate()
+      end,
+      desc = "Debug: Stop",
+    },
+    {
+      "<Leader>db",
+      function()
+        require("dap").toggle_breakpoint()
+      end,
+      desc = "Debug: Toggle Breakpoint",
+    },
+  },
   config = function()
     local dap = require("dap")
     local dapui = require("dapui")
+    local dap_python = require("dap-python")
 
-    -- Setup the UI
     dapui.setup()
 
-    -- Configure Mason-Nvim-Dap (The Bridge)
-    require("mason-nvim-dap").setup({
-      automatic_installation = true,
-      handlers = {},
-      ensure_installed = {
-        -- Add debuggers you need here if Mason doesn't auto-install them
-        -- "python",
-        -- "cpptools",
-      },
-    })
+    local venv_python = vim.fn.getcwd() .. "/.venv/bin/python"
+    if vim.fn.executable(venv_python) == 1 then
+      dap_python.setup(venv_python)
+    else
+      dap_python.setup("python3")
+    end
 
-    -- Listeners to open UI automatically
     dap.listeners.before.attach.dapui_config = function()
       dapui.open()
     end
@@ -38,8 +76,14 @@ return {
       dapui.close()
     end
 
-    -- Keymaps
-    vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
-    vim.keymap.set("n", "<Leader>dc", dap.continue, { desc = "Continue" })
+    local project_config = vim.fn.getcwd() .. "/.nvim.lua"
+    if vim.fn.filereadable(project_config) == 1 then
+      local success, err = pcall(dofile, project_config)
+      if success then
+        vim.notify("Config has loaded!", vim.log.levels.INFO)
+      else
+        vim.notify("Error at .nvim.lua: " .. err, vim.log.levels.ERROR)
+      end
+    end
   end,
 }
